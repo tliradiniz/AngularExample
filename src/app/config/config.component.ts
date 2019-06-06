62d092c1005c3e411f3d9664f5694bfcbcd4c57f
tliradiniz <xseed@LAPTOP-NO28M498.localdomain>
2019-06-06 13:49:22 +0000

    Atualização Lib

--- src/app/config/config.component.ts
+++ src/app/config/config.component.ts
@@ -1,58 +1,54 @@
-import { Component } from '@angular/core';
+import { HttpTestService } from './http-test.service';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import {Http, Response} from '@angular/http';
 import {Headers} from '@angular/http';
 import { map } from 'rxjs/operators';
+import { HttpHeaders } from '@angular/common/http';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-config',
-  templateUrl: './config.component.html',
-  providers: [],
+  templateUrl: './funcionario.html',
+  providers: [HttpTestService],
   styleUrls: ['./config.component.css']
 })
 export class ConfigComponent {
 
+
   data: string;
-  loading: boolean;
-
-   constructor(private http: Http){}
-
-    delay(ms: number) {
-      return new Promise( resolve => setTimeout(resolve, ms) );
-    }
-
-   async onTestPost(text: string, link: string) {
-    this.postJSON(text, link)
-    .subscribe(
-       error => JSON.parse(JSON.stringify(error)),
-       () => console.log("acesso a webapi post ok...")
-    );
-    await this.delay(100);
-    this.makeRequest(link);
-}
-
-  postJSON(txt: string, link: string) {
-    let json = JSON.stringify({field: txt});
-    let params = 'json=' + json;
-    let cabe = new Headers();
-    cabe.append('Content-Type', 'application/json');
-    return this.http.post(link,
-    params, {
-             headers : cabe
-            })
-            .pipe(map(res => res.json()));
-}
+  postForm: FormGroup;
 
 
-  makeRequest(link: string): void {
-    this.loading = true;
-    this.http.request(link)
-    .subscribe((res: Response) => {
-      this.data = res.json();
-      this.loading = false;
-    });
+  delay(ms: number) {
+    return new Promise( resolve => setTimeout(resolve, ms) );
+  }
+
+
+  constructor(private httpService: HttpTestService) {
+
+  }
+  async onTestPost(link: string) {
+
+   this.httpService.requestPost(link, this.postForm);
+   await this.delay(100);
+   this.httpService.getJSON(link);
+   await this.delay(100);
+   this.data = this.httpService.data;
+
+  }
+
+  request(link: string) {
+    this.httpService.getJSON(link);
   }
 
   ngOnInit() {
+
+    this.postForm = new FormGroup({
+      matricula: new FormControl(''),
+      nome: new FormControl(''),
+      salario: new FormControl('')
+
+   });
   }
 }
 
--- src/app/config/http-test.service.ts
+++ src/app/config/http-test.service.ts
@@ -1,21 +1,48 @@
-import { Injectable } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { Injectable, Output, EventEmitter, Input } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import {Headers} from '@angular/http';
 import { map } from 'rxjs/operators';
 
 @Injectable()
 
+
 export class HttpTestService {
-  constructor(private _http: Http) { }
-       postJSON(txt: string) {
-                let json = JSON.stringify({field: txt});
-                let params = 'json=' + json;
-                let cabe = new Headers();
-                cabe.append('Content-Type', 'application/json');
-                return this._http.post('http://localhost:8080/SWBackend/jaxrs/Person',
-                params, {
-                         headers : cabe
-                        })
-                        .pipe(map(res => res.json()));
-            }
+  data: any;
+  loading: boolean;
+
+  constructor(private http: Http) { }
+
+
+        requestPost(link: any, postForm: FormGroup){
+          this.postJSON(link, postForm)
+         .subscribe(
+          error => JSON.parse(JSON.stringify(error)),
+          () => console.log('acesso a webapi post ok...')
+          );
+        }
+        postJSON(link: string, postForm: FormGroup) {
+          console.log(postForm.value);
+            let json = JSON.stringify(postForm.value);
+            let params = 'json=' + json;
+            let cabe = new Headers();
+            cabe.append('Content-Type', 'application/json');
+            return this.http.post(link,
+            params, {
+                    headers : cabe
+                    })
+                    .pipe(map(res => res.json()));
+          }
+
+
+      getJSON(link: string): void {
+        this.loading = true;
+        this.http.request(link)
+        .subscribe((res: Response) => {
+          console.log(res.json());
+          this.data = res.json();
+          this.loading = false;
+
+        });
+      }
 }
